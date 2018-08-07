fa69ca8975fa3e4536e5f196a216a916aa7d6bd3
kwong991 <kgwong991@gmail.com>
2018-08-07 14:42:51 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,6 @@
 # write your method here
 
-def crazy_strings(food,drink)
+def crazy_strings(fosod,drink)
 food = food.reverse.upcase
 drink = drink.swapcase.gsub"(z)","(s)"
 return "#{food} #{drink}"
