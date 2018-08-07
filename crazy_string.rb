6d3e6f08aa3a58cbb0dcc3789ac4210ebc837926
kwong991 <kgwong991@gmail.com>
2018-08-07 14:40:12 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,7 @@
 # write your method here
 
 def crazy_strings(food,drink)
-puts "I like to eat #{food}".reverse.upcase
-puts "I love to drink #{drink}".swapcase.gsub"(s)","(z)"
+food = food.reverse.upcase
+drink = drink.swapcase.gsub"(s)","(z)"
 return "#{food} #{drink}"
 end
